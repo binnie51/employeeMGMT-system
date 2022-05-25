49a81f4381bd7d8e2c01895e49557b5e5f91fede
Vincent Tjia <vtjia51@gmail.com>
2022-05-25 03:19:50 -0400

    add schema and pre-populate seeds

---
+++ server.js
@@ -0,0 +1,40 @@
+// Add dependencies
+const mysql = require('mysql2');
+const inquirer = require('require');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employee_management_db'
+    }.
+    console.log(`Connected to the employee_management_db database.`)
+);
+
+function init() {
+    inquirer
+    .prompt({
+        name: 'action-menu',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            "View All Employees",
+            "Add Employees",
+            "Update Employee Roles",
+            "View All Roles",
+            "Add Role",
+            "View All Department",
+            "Add Department"
+        ]
+    })
+    .then( (answer) => {
+        switch (answer.action-menu) {
+            case "View All Department":
+                viewDept();
+                break;
+            case "Add Employees":
+                addEmployee()
+        }
+    })
+}
